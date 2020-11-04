1f369fbbfc6d6ef76e040ad58fdfbe5e25ef643d
Robert Mills <bob@blogbybob.com>
2020-11-04 17:20:44 -0500

    add some logging

--- app/src/index.ts
+++ app/src/index.ts
@@ -8,10 +8,13 @@ const connectionManager = getConnectionManager()
 
 async function start () {
     //logged query is id IN []
+    console.log('THIS METHOD IS CALLING INTO SHARED FOR BOTH GETTING THE CONNECTION AND RUNNING THE QUERY AND PROPERLY TRANSLATES INTO "ID IN"')
     await getusersConnectionFromSharedRunningQueryInShared()
     //logged query is id IN []
+    console.log('THIS METHOD IS NOT CALLING SHARED FOR GETTING THE CONNECTION NOR RUNNING THE QUERY AND PROPERLY TRANSLATES INTO "ID IN"')
     await getUsersUsingConnectionOutsideOfSharedAndRunningQueryOutsideOfShared()
     //logged query is id = ? 
+    console.log('THIS METHOD IS CALLING INTO SHARED TO GET A CONNECTION BUT RUNNING THE QUERY NOT IN SHARED AND TRANSLATES TO "ID = ?" INSTEAD OF IN')
     await getUsersUsingConnectionFromSharedConnectionRunningQueryOutsideOfShared()
 }
 
